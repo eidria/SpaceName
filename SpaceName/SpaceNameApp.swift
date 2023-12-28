9d8584c1fa27acea88ccc78e5e24ada78fa9667b
Dan Galbraith <dan@eidria.com>
2023-12-28 07:44:26 -0800

    Initial Commit

---
+++ SpaceName/SpaceNameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceNameApp.swift
+//  SpaceName
+//
+//  Created by Dan Galbraith on 12/28/23.
+//
+
+import SwiftUI
+
+@main
+struct SpaceNameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
